fe52890763a58f84fa59bd64a419b5d2b2ada77f
DeepanshuDabas03 <104428415+DeepanshuDabas03@users.noreply.github.com>
2022-10-14 23:11:22 +0530

    date.c

---
+++ date.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc,char **array) {
+    if(array[1]==NULL){
+        time_t now=time(NULL);
+        char *string_now=ctime(&now);
+        printf("%s",string_now);
+    }
+    else if(strcmp(array[1],"-u")==0){
+        time_t t=time(&t);
+		struct tm *utc=gmtime(&t);
+		if(utc==NULL){
+			printf("date:error unable to handle process right now \n");
+		}
+		printf("UTC TIME : %s\n",asctime(utc));
+    }
+    else if(strcmp(array[1],"-r")==0){
+        if(argc<3){
+            printf("date: option requires an argument -- '%s'\n",array[1]);
+            return 0;
+        }
+        struct stat attr;
+        stat(array[2], &attr);
+        printf("Last modified time: %s", ctime(&attr.st_mtime));
+    }
+    else{
+        printf("date: error invalid option\n");
+    }
+    
+return 0;
+}
